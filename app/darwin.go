b8b34495f1c1f73b4ea1d771dfea8f06a37bdbb5
whiler <9418312+whiler@users.noreply.github.com>
2023-03-29 22:48:43 +0800

    15 icon macos (#19)
    * 16 icon windows (#18)
    * ignore empty repo
    * 调整作为 macOS APP 运行时的工作路径

---
+++ app/darwin.go
@@ -0,0 +1,69 @@
+//go:build darwin
+// +build darwin
+
+// macOS App 运行时的工作路径是`/` 。
+// 使用 `~/Library/Caches/kmactor` 作为工作路径，并将需要的资源文件复制到工作路径下。
+
+package app
+
+import (
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+var files = []string{
+	"cert.pem",
+	"key.pem",
+	"repo.txt",
+}
+
+func Initialize() error {
+	if path, err := os.Executable(); err != nil {
+		return err
+	} else if cacheDir, err := os.UserCacheDir(); err != nil {
+		return err
+	} else {
+		contentsPath := filepath.Join(path, "../..")
+		base := filepath.Base(filepath.Join(contentsPath, ".."))
+		name := base[:strings.LastIndex(base, ".app")]
+		cachePath := filepath.Join(cacheDir, name)
+		if err = os.MkdirAll(cachePath, 0750); err != nil {
+			return err
+		} else if err = os.Chdir(cachePath); err != nil {
+			return err
+		} else {
+			for _, cur := range files {
+				src := filepath.Join(contentsPath, "Resources", cur)
+				dst := filepath.Join(cachePath, cur)
+				if !exists(src) || exists(dst) {
+					continue
+				} else if err = filecopy(dst, src); err != nil {
+					return err
+				}
+			}
+			return nil
+		}
+	}
+}
+
+func exists(path string) bool {
+	_, err := os.Stat(path)
+	return err == nil
+}
+
+func filecopy(dst, src string) error {
+	if r, err := os.Open(src); err != nil {
+		return err
+	} else {
+		defer r.Close()
+		if w, err := os.Create(dst); err != nil {
+			return err
+		} else {
+			defer w.Close()
+			_, err = io.Copy(w, r)
+			return err
+		}
+	}
+}
--- go.mod
+++ go.mod
@@ -1,15 +1,15 @@
 module kmactor
 
-go 1.19
+go 1.20
 
 require (
 	github.com/gorilla/websocket v1.5.0
 	github.com/whiler/robotgo v0.100.11-0.20230219231602-c06ea406f03a
-	golang.org/x/net v0.7.0
+	golang.org/x/net v0.8.0
 )
 
 require (
 	github.com/lxn/win v0.0.0-20210218163916-a377121e959e // indirect
-	golang.org/x/sys v0.5.0 // indirect
-	golang.org/x/text v0.7.0 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/text v0.8.0 // indirect
 )
